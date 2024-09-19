8f81bcf511dad8640faa51154d2023d865f720f6
marcos <marcoslucero@ceroca.cat>
2024-09-19 19:30:31 +0200

    para que no salga la nave

--- Assets/Scripts/NauJugador.cs
+++ Assets/Scripts/NauJugador.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class NauJugador : MonoBehaviour
 {
     private float _vel;
+
+    private Vector2 minPantalla, maxPantalla;
+
+
     // Start is called before the first frame update
     void Start()
     {
         _vel = 8;
+        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0, 0)); //poniedno limites
+        maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1, 1)); //mas limites
+       
+        
+        minPantalla.x = minPantalla.x + 0.75f;  //parametros de la nave para que no quede media ala
+        maxPantalla.x = maxPantalla.x + -0.75f;
+        
+        minPantalla.y = minPantalla.y + 0.75f;
+        maxPantalla.y = maxPantalla.y + -0.75f;
     }
 
     // Update is called once per frame
@@ -18,10 +32,15 @@ public class NauJugador : MonoBehaviour
         float direcioIndicadaY = Input.GetAxisRaw("Vertical"); //* _vel * Time.deltaTime;
 
 
+
         Vector2 direcioIndicada = new Vector2(direcioIndicadaX, direcioIndicadaY).normalized;
         
         Vector2 novaPos = transform.position;
         novaPos = novaPos + direcioIndicada * _vel * Time.deltaTime;
+
+        novaPos.x = Mathf.Clamp(novaPos.x, minPantalla.x, maxPantalla.x);
+        novaPos.y = Mathf.Clamp(novaPos.y, minPantalla.y, maxPantalla.y);
+
         transform.position = novaPos;
 
 
