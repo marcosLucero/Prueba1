c424d17acfa48ea06d040fca6ebdb6b7d6ec43b8
marcos <marcoslucero@ceroca.cat>
2024-09-26 17:01:53 +0200

    Midas pantalla nau
    peor manera

--- Assets/Scripts/NauJugador.cs
+++ Assets/Scripts/NauJugador.cs
@@ -15,25 +15,34 @@ public class NauJugador : MonoBehaviour
     void Start()
     {
         _vel = 8;
-        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0, 0)); //poniedno limites
+        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0, 0)); //poniedno limites de la pantalla
         maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1, 1)); //mas limites
+
+        float midaMeitatImatgeX = GetComponent<SpriteRenderer>().sprite.bounds.size.x  * transform.localScale.x / 2; //version larga
+        float midaMeitatImatgeY = GetComponent<SpriteRenderer>().bounds.size.y / 2; //version corta
 
 
         // minPantalla.x = minPantalla.x + 0.60f;  //parametros de la nave para que no quede media ala de el eje X
         // maxPantalla.x = maxPantalla.x - 0.60f;
         // minPantalla.x += 0.75f;     //+= para cuando es la misma, queda mejor
 
-        // Ajusta los límites de la pantalla considerando el tamaño del sprite de la nave de todos los parametros
-        minPantalla.x += GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        // Ajusta los limites de la pantalla considerando el tamaÃ±o del sprite de la nave de todos los parametros
+        /*
+        minPantalla.x += GetComponent<SpriteRenderer>().bounds.size.x / 2;  //Parametros de las esquinas y de todos los ejes positivos y negativos
         maxPantalla.x -= GetComponent<SpriteRenderer>().bounds.size.x / 2;
         minPantalla.y += GetComponent<SpriteRenderer>().bounds.size.y / 2;
-        maxPantalla.y -= GetComponent<SpriteRenderer>().bounds.size.y / 2;
+        maxPantalla.y -= GetComponent<SpriteRenderer>().bounds.size.y / 2; */
 
        // minPantalla = new Vector2(minPantalla.x + naveAncho, minPantalla.y + naveAlto);
        // maxPantalla = new Vector2(maxPantalla.x - naveAncho, maxPantalla.y - naveAlto);
 
 
-        // minPantalla.y = minPantalla.y + 0.70f;  //parametros de la nave para que no quede media ala de el eje Y
+         minPantalla.x += midaMeitatImatgeX;
+         maxPantalla.x -= midaMeitatImatgeX;
+         minPantalla.y += midaMeitatImatgeY;
+         maxPantalla.y -= midaMeitatImatgeY;
+
+           //parametros de la nave para que no quede media ala de el eje Y
         // maxPantalla.y = maxPantalla.y - 0.70f;
     }
 
