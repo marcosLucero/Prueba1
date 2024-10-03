2f33e09f4cbb7082ded68552299b3a1be8e27365
marcos <marcoslucero@ceroca.cat>
2024-10-03 17:03:53 +0200

    Generar enemigos

--- Assets/Scripts/GeneradorNumeros.cs
+++ Assets/Scripts/GeneradorNumeros.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class GeneradorNumeros : MonoBehaviour
 {
     [SerializeField] private GameObject prefabNumero;
+    private Vector2 minPantalla, maxPantalla;
     // Start is called before the first frame update
     void Start()
     {
-        
+        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2( 0, 0));
+        maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2( 1, 1));
+
+        InvokeRepeating("GenerarNumero", 1f, 2f);
+    }
+    private void GenerarNumero()
+    {
+        GameObject numero =Instantiate(prefabNumero);
+        numero.transform.position = new Vector2( Random.Range(minPantalla.x ,maxPantalla.x), maxPantalla.y);
+
     }
 
     // Update is called once per frame
